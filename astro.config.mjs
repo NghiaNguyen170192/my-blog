892b031c61dc1648b7b4ea9349a3024fa0019bda
Nghĩa Nguyễn <nghia.nguyen170192@gmail.com>
2022-10-10 21:11:19 +0700

    resolve image url

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import netlify from '@astrojs/netlify/functions';
+import image from '@astrojs/image';
 
 // https://astro.build/config
 export default defineConfig({
 	site: "http://localhost:3000",
 	output: "static",
-	integrations: [mdx()],
+	integrations: [
+		mdx(), 
+		image({
+			serviceEntryPoint: '@astrojs/image/sharp'
+	  	})],
 	markdown: {
 		shikiConfig: {
 			theme: "dracula",
